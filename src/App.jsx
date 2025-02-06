7e504f5dcc5b35f63af4cc832aacfc9efc5037ea
shuvam773 <shuvam.sk25@gmail.com>
2025-02-07 02:28:50 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Experience from "./components/Experience";
+// import Experience from "./components/Experience";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Project from "./components/Project";
@@ -33,7 +33,7 @@ const App = () => {
         <Hero />
         <About />
         <Technology />
-        <Experience />
+        {/* <Experience /> */}
         <Project />
         <Contact />
       </div>
