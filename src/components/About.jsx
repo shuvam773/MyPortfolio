27b47e8b4f7d63f3c7cfd7c1c055b8ecf294f01d
shuvam773 <shuvam.sk25@gmail.com>
2025-04-28 21:10:19 +0530

    Tweaking parallax tilt

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,31 +1,53 @@
 import aboutImg from "../assets/about.png"
 import { ABOUT_TEXT } from "../constants"
-import {motion} from "motion/react"
+import { motion } from "motion/react"
+import Tilt from 'react-parallax-tilt' // import Tilt
 
 const About = () => {
   return (
-    <div className=" pb-4">
-        <h2 className="my-20 text-center text-4xl">About <span className="text-neutral-500">Me</span></h2>
-        <div className="flex p-4 flex-wrap">
-            <motion.div 
-            whileInView={{opacity:1, x:0}} 
-            initial={{opacity: 0, x: -100}}
-            transition = {{duration:0.5}}
-            className="w-full lg:w-1/2 lg:p-8">
-                <div className="flex items-center justify-center">
-                    <img className="rounded-lg transition-all duration-1000 grayscale hover:grayscale-0 hover:scale-110 cursor-pointer"  src={aboutImg} alt=""/>
-                </div>
-            </motion.div>
-            <motion.div 
-            whileInView={{opacity:1, x:0}}
-            initial={{opacity:0, x:100}}
-            transition={{duration:0.5}}
-            className="w-full flex items-center justify-center lg:w-1/2">
-                <div className="flex justify-center lg:justify-start">
-                    <p className="my-2 max-w-xl py-6 font-light tracking-tighter text-lg">{ABOUT_TEXT}</p>
-                </div>
-            </motion.div>
-        </div>
+    <div className="pb-4">
+      <h2 className="my-20 text-center text-4xl">
+        About <span className="text-neutral-500">Me</span>
+      </h2>
+      <div className="flex p-4 flex-wrap">
+        <motion.div 
+          whileInView={{ opacity: 1, x: 0 }} 
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 0.5 }}
+          className="w-full lg:w-1/2 lg:p-8"
+        >
+          <div className="flex p-8 justify-center">
+            <Tilt
+              glareEnable={true}
+              glareMaxOpacity={0.45}
+              scale={1.05}
+              tiltMaxAngleX={15}
+              tiltMaxAngleY={15}
+              transitionSpeed={2500}
+              className="rounded-2xl "
+            >
+              <img 
+                className="rounded-2xl shadow-inner"  
+                src={aboutImg} 
+                alt="About"
+              />
+            </Tilt>
+          </div>
+        </motion.div>
+
+        <motion.div 
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ duration: 0.5 }}
+          className="w-full flex items-center justify-center lg:w-1/2"
+        >
+          <div className="flex justify-center lg:justify-start">
+            <p className="my-2 max-w-xl py-6 font-light tracking-tighter text-lg">
+              {ABOUT_TEXT}
+            </p>
+          </div>
+        </motion.div>
+      </div>
     </div>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,54 +1,83 @@
-import { HERO_CONTENT } from "../constants"
-import profilePic from "../assets/skProfile.png"
-import { motion } from "motion/react"
-import { useTypewriter, Cursor } from 'react-simple-typewriter'
+import { HERO_CONTENT } from '../constants';
+import profilePic from '../assets/skProfile.png';
+import { motion } from 'motion/react';
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
+import Tilt from 'react-parallax-tilt';
 
-const container = (delay) =>({
-    hidden: {x: -100, opacity:0},
-        visible:{
-            x:0,
-            opacity: 1,
-            transition: {duration: 0.5, delay: delay},
-        }
-    }
-)
+const container = (delay) => ({
+  hidden: { x: -100, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.5, delay: delay },
+  },
+});
 const Hero = () => {
-    const [text] = useTypewriter({
-        words: ['Full Stack Developer', 'Programmer', 'Traveller'],
-        loop: true,
-        typeSpeed: 100,
-        deleteSpeed: 70,
-        delaySpeed: 1500,
-    })
-    
+  const [text] = useTypewriter({
+    words: ['Full Stack Developer', 'Programmer', 'Traveller'],
+    loop: true,
+    typeSpeed: 100,
+    deleteSpeed: 70,
+    delaySpeed: 1500,
+  });
+
   return (
     <div className=" pb-4 lg:mb-35">
-        <div className="flex p-4 flex-wrap">
-            <div className="w-full lg:w-1/2 ">
-                <div className="flex flex-col items-start lg:item-start">
-                    <motion.h1 
-                    variants={container(0)}
-                    initial="hidden"
-                    animate="visible"
-                    className="pb-10 text-6xl font-thin tracking-tight lg:mt-16 lg:text-7xl">Shuvam Kumar</motion.h1>
-                    <motion.span variants={container(0.5)}
-                    initial="hidden"
-                    animate="visible" className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent font-nova-cut">{text}<Cursor cursorColor='white'  cursorStyle='_' /></motion.span>
-                    <motion.p variants={container(1)}
-                    initial="hidden"
-                    animate="visible" className="my-2 max-w-xl py-6 font-light tracking-tighter text-lg">{HERO_CONTENT}</motion.p>
-                </div>
-            </div>
-            <div className="w-full  lg:w-1/2 lg:p-8 ">
-                <div className="flex p-8 justify-center">
-                    <motion.img initial={{x:100, opacity:0}}
-                    animate={{x:0, opacity:1}}
-                    transition={{duration:1, delay:1.2}}  className="rounded-2xl shadow-inner" src={profilePic} alt="Profile"/>
-                </div>
-            </div>
+      <div className="flex p-4 flex-wrap">
+        <div className="w-full lg:w-1/2 ">
+          <div className="flex flex-col items-start lg:item-start">
+            <motion.h1
+              variants={container(0)}
+              initial="hidden"
+              animate="visible"
+              className="pb-10 text-6xl font-thin tracking-tight lg:mt-16 lg:text-7xl"
+            >
+              Shuvam Kumar
+            </motion.h1>
+            <motion.span
+              variants={container(0.5)}
+              initial="hidden"
+              animate="visible"
+              className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent font-nova-cut"
+            >
+              {text}
+              <Cursor cursorColor="white" cursorStyle="_" />
+            </motion.span>
+            <motion.p
+              variants={container(1)}
+              initial="hidden"
+              animate="visible"
+              className="my-2 max-w-xl py-6 font-light tracking-tighter text-lg"
+            >
+              {HERO_CONTENT}
+            </motion.p>
+          </div>
         </div>
+        <div className="w-full  lg:w-1/2 lg:p-8 ">
+          <div className="flex p-8 justify-center">
+            <Tilt
+              glareEnable={true}
+              glareMaxOpacity={0.45}
+              scale={1.05}
+              tiltMaxAngleX={15}
+              tiltMaxAngleY={15}
+              transitionSpeed={2500}
+              className="rounded-2xl"
+            >
+              <motion.img
+                initial={{ x: 100, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 1, delay: 1.2 }}
+                className="rounded-2xl shadow-inner"
+                src={profilePic}
+                alt="Profile"
+              />
+            </Tilt>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Hero;
